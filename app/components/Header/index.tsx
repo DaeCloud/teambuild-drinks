6857a6e8dddafbbc41465acbfcebad757ac2ec66
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2025-11-03 17:21:42 +0200

    Updated favicon

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,20 +1,22 @@
 'use client';
 
-export default function Header() {
-    function handleLogin() {
-        // Handle admin login logic
-        alert('Admin login clicked');
-    }
+import Image from 'next/image';
 
+export default function Header() {
   return (
     <header className="w-full py-6 px-16 bg-gray-900 border-b border-black/[.08] dark:border-white/[.145] flex items-center justify-between">
-      <h1 className="text-2xl font-bold text-zinc-50">
-        Drink Preferences for Team Build
-      </h1>
-
-        {/* <button className="mt-4 px-4 py-2 bg-foreground text-background rounded-full hover:bg-[#383838] dark:hover:bg-[#ccc] hover:cursor-pointer" onClick={handleLogin}>
-          Admin Login
-        </button> */}
+      <div className="flex items-center gap-3">
+        <Image
+          src="/icon.png"
+          alt="Team Build Icon"
+          className="w-20 h-20 rounded-md"
+          width={80}
+          height={80}
+        />
+        <h1 className="text-2xl font-bold text-zinc-50">
+          Drink Preferences for Team Build
+        </h1>
+      </div>
     </header>
   );
 }
